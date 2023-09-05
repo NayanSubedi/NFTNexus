18d710f7de9a1abd89f1295e9740500f9e9439bd
Nayan Subedi <nayansubedi365@gmail.com>
2023-09-05 12:34:53 +0545

    h1,p updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,15 @@
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="flex flex-col md:flex-row w-4/5 justify-between items-center mx-auto py-10">
+        <div className="md:w-3/6 w-full">
+            <div className="text-white text-5xl font-bold">
+                <h1>Collect and Create <br /> Pixelated Arts,<br />
+                <span className="text-gradient">NFTs</span> Collections</h1>
+                <p className="text-gray-400 font-semibold text-lg ">From Pixels to Possession.</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
