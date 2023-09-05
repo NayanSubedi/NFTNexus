50b4ea8502432b39967bb3f29ca0d40e42064313
Nayan Subedi <nayansubedi365@gmail.com>
2023-09-05 12:42:11 +0545

    button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,9 @@ const Hero = () => {
                 <span className="text-gradient">NFTs</span> Collections</h1>
                 <p className="text-gray-400 font-semibold text-lg ">From Pixels to Possession.</p>
             </div>
+        <div className="flex mt-5">
+            <button className="shadow-xl shadow-black text-white bg-[#1526bd] hover:bg-[#b45609] md:text-xs p-2 rounded-full">Create NFT</button>
+        </div>
         </div>
     </div>
   )
