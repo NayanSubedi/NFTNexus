e4225f9dd14a6c54a9ffe8215b04def099761645
Nayan Subedi <nayansubedi365@gmail.com>
2023-09-05 10:49:38 +0545

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import NFTNexuslogo from '../assets/NFTNexus.png'
 
 const Header = () => {
   return (
-    <div className='w-4/5 flex'>
-        <div>
-            <img src={NFTNexuslogo} alt='logo' />
+    <div className='w-4/5 flex justify-between md:justify-center items-center py-4 mx-auto '>
+        <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+            <img className='w-40 cursor-pointer' src={NFTNexuslogo} alt='logo' />
         </div>
     </div>
   )
