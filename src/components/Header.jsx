05916251bfec2d719e81af0c0bc5c21a5c283534
Nayan Subedi <nayansubedi365@gmail.com>
2023-09-05 11:37:31 +0545

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
             <li className='mx-4 cursor-pointer'>Features</li>
         </ul>
 
-        <button className='shadow-s shadow-blue text-white bg-[#b90a0a] hover:bg-[#5C469C] md:text-xs p-2 rounded-full'>
+        <button className='shadow-s shadow-blue text-white bg-[#5C469C] hover:bg-[#122b28] md:text-xs p-2 rounded-full'>
             Connect Wallet
         </button>
     </div>
