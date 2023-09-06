032b08acf66c673edc82df885560dc4723aa2390
Nayan Subedi <nayansubedi365@gmail.com>
2023-09-06 07:34:03 +0545

    imported footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import Hero from "./components/Hero"
 import Artworks from "./components/Artworks"
 import Transactions from "./components/Transactions"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
