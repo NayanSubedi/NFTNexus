4c1dee75aa7af87635af1b933f3c6cd174e423a7
Nayan Subedi <nayansubedi365@gmail.com>
2023-09-06 17:45:18 +0545

    new show modal

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,6 +1,7 @@
 import { createGlobalState } from "react-hooks-global-state"
 const {setGlobalState, useGlobalState, getGlobalState} = createGlobalState({
     modal: 'scale-0',
+    showModal: 'scale-0'
 
 })
 
