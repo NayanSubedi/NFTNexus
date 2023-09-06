6aa660cf2ec97b4aa4d7474c97055e49b4b2279e
Nayan Subedi <nayansubedi365@gmail.com>
2023-09-06 07:34:27 +0545

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,34 @@
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+import NFTNexuslogo from '../assets/NFTNexus.png'
 
-export default Footer
+const Footer = () => (
+    <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+      <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+        <div className="flex flex-[0.25] justify-center items-center">
+          <img src={NFTNexuslogo} alt="logo" className="w-32" />
+        </div>
+  
+        <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Market
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Artist
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Features
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Community
+          </p>
+        </div>
+  
+        <div className="flex flex-[0.25] justify-center items-center">
+          <p className="text-white text-right text-xs">
+            &copy;2023 All rights reserved
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+  
+  export default Footer
