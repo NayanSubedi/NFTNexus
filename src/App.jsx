c184f3782557721b9e42df478da6d244e8b18714
Nayan Subedi <nayansubedi365@gmail.com>
2023-09-24 16:37:40 +0545

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import Header from "./components/Header"
-import Hero from "./components/Hero"
-import Artworks from "./components/Artworks"
-import Transactions from "./components/Transactions"
-import Footer from "./components/Footer"
-import CreateNFT from "./components/CreateNFT"
-import ShowNFT from "./components/ShowNFT"
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Link, animateScroll as scroll } from 'react-scroll'; 
+import Home from './components/Home';
+import Artworks from './components/Artworks';
+import Creators from './components/Creators';
+import Transactions from './components/Transactions';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import UpdateNFT from './components/UpdateNFT';
+import ShowNFT from './components/ShowNFT'
+
 
 const App = () => {
   return (
     <div className="min-h-screen">
-      <div className="gradient-bg-hero"> 
-      <Header />
-      <Hero />
-      </div>
+      <div className="gradient-bg-hero">
+        <Header />
+        <Home />
+        <UpdateNFT />
       <Artworks />
-      <Transactions />
-      <Footer />
-      <CreateNFT />
       <ShowNFT />
-    </div>
-  )
-}
+      <Creators />
+      <Transactions />
 
-export default App
+      <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default App;
