a3a5ed1aa4982fbcef7a316b02fbcde3b8eb6521
Nayan Subedi <nayansubedi365@gmail.com>
2023-09-25 18:26:45 +0545

    Final stage

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import ArtworksPage from './routes/ArtworksPage';
+import CreatorsPage from './routes/CreatorsPage';
+import TransactionsPage from './routes/TransactionsPage';
+
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/artworks' element={<ArtworksPage />} />
+      <Route path='/creators' element={<CreatorsPage />} />
+      <Route path='/transactions' element={<TransactionsPage />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
+
