c24ef295eaac941228efda72b03e72d342cbea15
Nayan Subedi <nayansubedi365@gmail.com>
2023-09-25 14:13:33 +0545

    added transactions

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -6,7 +6,7 @@ const Transactions = () => {
         <div className="w-4/5 py-10 mx-auto">
             <h4 className="text-white text-3xl font-bold uppercase text-gradient">Latest Transactions</h4>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gaps-4 lg:gaps-2 py-2.5">
-          {Array(3)
+          {Array(9)
             .fill()
             .map((nft, i) => (
             //   <Card key={i} nft={i + 1} />
