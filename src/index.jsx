1a716575df10107e423d63c4f97f483c1cf5545c
Nayan Subedi <nayansubedi365@gmail.com>
2023-09-26 15:27:31 +0545

    index.jsx update

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import ArtworksPage from './routes/ArtworksPage';
 import CreatorsPage from './routes/CreatorsPage';
 import TransactionsPage from './routes/TransactionsPage';
-import PriceFeed from './components/PriceFeed';
+
 
 
 
@@ -18,7 +18,6 @@ ReactDOM.render(
       <Route path='/artworks' element={<ArtworksPage />} />
       <Route path='/creators' element={<CreatorsPage />} />
       <Route path='/transactions' element={<TransactionsPage />} />
-      <Route path='/pricefeed' element={<PriceFeed />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
