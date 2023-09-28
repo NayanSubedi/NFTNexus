1fd9bc128a7d29296637e32cae4f89e755259fc5
Nayan Subedi <nayansubedi365@gmail.com>
2023-09-28 18:18:23 +0545

    Commit

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BiTransfer } from 'react-icons/bi';
 import { MdOpenInNew } from 'react-icons/md';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import './Glow.css';
 
 const Transactions = () => {
   const [showChart, setShowChart] = useState(false);
   const [cryptoData, setCryptoData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const rowsPerPage = 10;
 
   const toggleChart = () => {
     setShowChart(!showChart);
@@ -19,7 +22,7 @@ const Transactions = () => {
         {
           params: {
             vs_currency: 'usd',
-            ids: 'bitcoin,ethereum,chainlink,ripple,fantom,solana,tron,litecoin,dogecoin,aave,algorand,cosmos,cardano,filecoin,polkadot,uniswap,hedera,okb',
+            ids: 'bitcoin,binancecoin,hedera-hashgraph,terra-luna-2,the-open-network,internet-computer,tether,the-sandbox,tezos, pepe,chiliz,stellar,shiba-inu,avalanche-2,BNB,1inch,ethereum,chainlink,ripple,fantom,solana,tron,litecoin,dogecoin,aave,algorand,cosmos,cardano,filecoin,polkadot,uniswap,hedera,okb',
           },
         }
       );
@@ -53,6 +56,25 @@ const Transactions = () => {
 
   const ethAmounts = ['56 ETH', '75 ETH', '42 ETH', '69 ETH', '17 ETH', '5 ETH'];
 
+  const totalPages = Math.ceil(cryptoData.length / rowsPerPage);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const startIndex = (currentPage - 1) * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+
+  const visibleCryptoData = cryptoData.slice(startIndex, endIndex);
+
   return (
     <div className='bg-[#24034b]'>
       <div className="w-4/5 mx-auto py-24">
@@ -72,7 +94,17 @@ const Transactions = () => {
             {showChart ? 'View Less' : 'View More'}
           </button>
         </div>
-        {showChart && <CryptoPriceTable cryptoData={cryptoData} />}
+        {showChart && (
+          <CryptoPriceTable
+            cryptoData={cryptoData}
+            currentPage={currentPage}
+            rowsPerPage={rowsPerPage}
+            setCurrentPage={setCurrentPage}
+            totalPages={totalPages}
+            handleNextPage={handleNextPage}
+            handlePrevPage={handlePrevPage}
+          />
+        )}
       </div>
     </div>
   );
@@ -95,7 +127,15 @@ const Transaction = ({ tx, address, ethAmount }) => (
   </div>
 );
 
-const CryptoPriceTable = ({ cryptoData }) => {
+const CryptoPriceTable = ({
+  cryptoData,
+  currentPage,
+  rowsPerPage,
+  setCurrentPage,
+  totalPages,
+  handleNextPage,
+  handlePrevPage,
+}) => {
   const [cryptoLogos, setCryptoLogos] = useState({});
 
   useEffect(() => {
@@ -106,7 +146,7 @@ const CryptoPriceTable = ({ cryptoData }) => {
           {
             params: {
               vs_currency: 'usd',
-              ids: 'bitcoin,ethereum,chainlink,ripple,fantom,solana,tron,litecoin,dogecoin,aave,algorand,cosmos,cardano,filecoin,polkadot,uniswap,hedera,okb',
+              ids: 'bitcoin,binancecoin,stellar,terra-luna-2,hedera-hashgraph,the-open-network,tether,internet-computer,tezos,pepe, the-sandbox,chiliz,shiba-inu,avalanche-2,BNB,1inch,ethereum,chainlink,ripple,fantom,solana,tron,litecoin,dogecoin,aave,algorand,cosmos,cardano,filecoin,polkadot,uniswap,hedera,okb',
             },
           }
         );
@@ -125,6 +165,11 @@ const CryptoPriceTable = ({ cryptoData }) => {
     fetchCryptoLogos();
   }, []);
 
+  const startIndex = (currentPage - 1) * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+
+  const visibleCryptoData = cryptoData.slice(startIndex, endIndex);
+
   return (
     <div className="mt-5">
       <h4 className="text-white text-3xl font-bold uppercase text-gradient">
@@ -134,17 +179,17 @@ const CryptoPriceTable = ({ cryptoData }) => {
         <table className="min-w-full border gradient-bg-hero border-violet-800  glow">
           <thead>
             <tr className='border-violet-800'>
-              <th className="px-4 py-2  bg-gray-800 text-gray-300"></th> 
+              <th className="px-4 py-2  bg-gray-800 text-gray-300"></th>
               <th className="px-4 py-2  bg-gray-800 text-gray-300">Crypto Name</th>
               <th className="px-4 py-2 bg-gray-800 text-gray-300">Price (USD)</th>
               <th className="px-4 py-2 bg-gray-800 text-gray-300">Price Change</th>
             </tr>
           </thead>
           <tbody>
-            {cryptoData.map((crypto, index) => (
+            {visibleCryptoData.map((crypto, index) => (
               <tr
                 key={index}
-                style={{ borderBottom: '1px solid #e2e8f0' }} 
+                style={{ borderBottom: '1px solid #e2e8f0' }}
               >
                 <td className="px-4 py-2 text-center">
                   {cryptoLogos[crypto.id] && (
@@ -156,18 +201,47 @@ const CryptoPriceTable = ({ cryptoData }) => {
                   )}
                 </td>
                 <td className="px-4 text-white font-semibold text-center py-2">{crypto.name}</td>
-                <td className="px-4 text-white text-center font-semibold py-2">${crypto.current_price.toFixed(2)}</td>
+                <td className="px-4 text-white text-center font-semibold py-2">${crypto.current_price.toFixed(7)}</td>
                 <td
                   className={`px-4 py-2 text-center ${
                     crypto.price_change_percentage_24h >= 0 ? 'text-green-500' : 'text-red-500'
                   }`}
                 >
-                  {crypto.price_change_percentage_24h.toFixed(2)}%
+                  {crypto.price_change_percentage_24h.toFixed(4)}%
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
+        <div className="text-center my-5">
+          <button
+            className='shadow-lg shadow-black font-bold text-3xl text-[#1526bd] hover:text-[#b45609] text-white rounded-full px-1.5 py-1'
+            onClick={handlePrevPage}
+            disabled={currentPage === 1}
+          >
+            <IoIosArrowBack />
+          </button>
+          {Array.from({ length: totalPages }, (_, i) => (
+            <button
+              key={i}
+              className={`${
+                i + 1 === currentPage
+                  ? ' text-white'
+                  : ' text-gray-300 hover:bg-[#1526bd] hover:text-white'
+              }  font-light text-2xl px-2 rounded-xl mx-1`}
+              onClick={() => setCurrentPage(i + 1)}
+            >
+              {i + 1}
+            </button>
+          ))}
+          <button
+            className='shadow-lg shadow-black font-bold text-3xl text-[#1526bd] hover:text-[#b45609] text-white rounded-full px-1.5 py-1'
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+          >
+            <IoIosArrowForward />
+          </button>
+        </div>
       </div>
     </div>
   );
